c5f4f06d3ef410157d9be82e4d6ba4d5b1ef720d
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-09-12 12:23:04 -0300

    091224 aula0202

--- js/api.js
+++ js/api.js
@@ -6,10 +6,29 @@ const api = {
         }
         catch {
             alert('Erro ao buscar pensamentos.')
-            throw error
-            
+            throw error  
+        }
+    },
+
+    async salvarPensamento(pensamento) {
+        try {
+            const response = await fetch ('http://localhost:3000/pensamentos', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+        }
+        catch {
+            alert('Erro ao buscar pensamentos.')
+            throw error  
         }
     }
+
+
 }
 
 export default api;
+
